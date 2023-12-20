c86dc6d63a5bc4e9057118996db72ca91ee47da1
Juke <840962517@qq.com>
2023-12-20 11:04:53 +0800

    init

---
+++ swift-flutter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-flutter
+//
+//  Created by jushaocong on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-flutter/swift_flutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_flutterApp.swift
+//  swift-flutter
+//
+//  Created by jushaocong on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct swift_flutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
